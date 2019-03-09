02c8f7fbcb107219aa94017115c19286be5eea4a
tuan <ale33@mail.ccsf.edu>
2019-03-08 21:04:34 -0800

    AdTractor deploys AdTract, business can deploy multiple contracts

--- contracts/AdTract.sol
+++ contracts/AdTract.sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.5.4;
 
-
 contract AdTract{
     
-    // optional: uint private funds;
+    //optional: uint private funds;
     mapping(address => address payable) private referrals;
     address payable private owner;
+    address private AdTractor;
     uint percentageReward;
     
+    constructor() public{
+        AdTractor = msg.sender;
+    }
     //refer customer 
     function refer(address customer) public{
         referrals[customer] = msg.sender;
@@ -19,31 +22,26 @@ contract AdTract{
         //look at referrals, match customer to advertiser
         address payable advertiser = referrals[msg.sender];
         
-        //calculate reward
-        uint reward = msg.value*percentageReward/100;
+        //can be set percentage or set value
+        uint advertiserReward = msg.value*percentageReward/100;
                 
         //pay advertiser  
-        advertiser.transfer(reward);
-        
-        //pay AdTractor
+        advertiser.transfer(advertiserReward);
         
         //transfer remaining transaction to business
-        owner.transfer(msg.value - reward);
+        owner.transfer(msg.value - advertiserReward);
     }
     
     //will set owner of this contract in AdTractor contract
-    function setOwner(address payable _owner) public{
+    function setOwner(address payable _owner) external{
         //only Adtractor can assign owner
-        //require(AdTractor address == msg.sender);
-        
+        require(AdTractor == msg.sender);
         owner = _owner;
     }
     
     //set advertiser's reward percentage
-    function setPercentageReward(uint value) private{
-        require(owner == msg.sender);
+    function setPercentageReward(uint value) external{
+        require(AdTractor == msg.sender);
         percentageReward = value;
     }
-    
-    
 }
