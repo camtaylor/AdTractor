9fdca75565d9fdae633dc74cc34c262e086652d4
Cameron Taylor <camerontaylor00@gmail.com>
2019-03-27 17:35:50 -0700

    Added list of adtracts, gitignore

--- contracts/AdTractor_AdTract.sol
+++ contracts/AdTractor_AdTract.sol
@@ -1,27 +1,16 @@
 pragma solidity ^0.5.4;
 
 contract AdTractor{
-    //mapping of contracts
-    mapping(address => _business) private businesses;
-    //keep track of businesses that deploy AdTract
-    address[] business;
-    
-    struct _business{
-        address[] contractsAddress;
-    }
-    
+
+    // List of deployed AdTracts
+    address[] public adtracts;
+
     //deploy new AdTrac, track business and contracts
     function newAdTract(uint percentageReward) public{
-        business.push(msg.sender);
         AdTract ad = new AdTract(percentageReward, msg.sender);
-        _business storage b = businesses[msg.sender];
-        b.contractsAddress.push(address(ad)); 
+        adtracts.push(ad);
     }
-    
-    //get contracts that the business deployed    
-    function getContracts() public view returns(address[] memory){
-        return businesses[msg.sender].contractsAddress;
-    }
+
 }
 
 contract AdTract{
